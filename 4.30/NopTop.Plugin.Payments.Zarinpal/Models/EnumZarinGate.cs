230e00c04ddee2a67d45873729ab3e8d0d4c738f
mohammad mokhtari <mohammad.mokhtari86@gmail.com>
2020-08-01 22:53:05 +0430

    Migrate to NopCommerce V4.30

---
+++ 4.30/NopTop.Plugin.Payments.Zarinpal/Models/EnumZarinGate.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NopTop.Plugin.Payments.Zarinpal.Models
+{
+
+    public enum EnumZarinGate
+    {
+        //Automatically choose the best
+        [Display(Name = "Zarin Gate")]
+        ZarinGate = 0,
+        //Persian Switch
+        [Display(Name = "Asan Pardakht")]
+        Asan = 1,
+        //Saman
+        [Display(Name = "Saman")]
+        Sep = 2,
+        //Sadad
+        [Display(Name = "Sadad")]
+        Sad = 3,
+        //Parsian
+        [Display(Name = "Parsian")]
+        Pec = 4,
+        //FanavaCart
+        [Display(Name = "FanavaCart")]
+        Fan = 5,
+        //Emtiaz
+        [Display(Name = "Emtiaz")]
+        Emz = 6,
+    }
+}
